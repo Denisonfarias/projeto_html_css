a70c39193e99b78391fb43f3f458c6e2f1a94ca4
Denisonfarias <denisonfarias@gmail.com>
2026-05-24 17:04:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const venues = [
   { id:13, name:"Mercado dos Peixes de Fortaleza", category:"Barracas", description:"Mercado tradicional de peixes e frutos do mar na Beira Mar.", neighborhood:"Meireles", rating:4.4, priceLevel:2, image:"assets/venue-barraca.jpg", hours:"06h - 22h" },
   
   // BARES
-  { id:14, name:"Bar da Gelada", category:"Bares", description:"Bar animado com refeição no local, viagem e clima descontraído.", neighborhood:"Parque Dois Irmãos", rating:4.4, priceLevel:2, image:"assets/venue-bar.jpg", hours:"17h - 02h" },
+  { id:14, name:"Bar da Gelada", category:"Bares", description:"Bar animado com refeição no local, viagem e clima descontraído.", neighborhood:"Parque Dois Irmãos", rating:4.4, priceLevel:2, image:"assets/bdg.png", hours:"17h - 02h" },
   { id:15, name:"Arena Bar & Petiscaria", category:"Bares", description:"Bar e grill com petiscos variados e ambiente acolhedor.", neighborhood:"Demócrito Rocha", rating:4.5, priceLevel:2, image:"assets/venue-bar.jpg", hours:"17h - 00h" },
   { id:16, name:"Samba do Vila", category:"Bares", description:"Amplo espaço com rodas de samba, porções, caipirinhas e clima animado.", neighborhood:"Varjota", rating:4.2, priceLevel:2, image:"assets/venue-musica.jpg", hours:"17h - 02h" },
   { id:17, name:"Boteco do Imprensa", category:"Bares", description:"Boteco tradicional com petiscos, cerveja gelada e clima descontraído.", neighborhood:"Aldeota", rating:4.1, priceLevel:2, image:"assets/venue-bar.jpg", hours:"17h - 00h" },
